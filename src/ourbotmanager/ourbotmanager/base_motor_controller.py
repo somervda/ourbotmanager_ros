59cfbefe3a0466f6cea7541c12e179cde0361cd3
somervda <davidsomerville@comcast.net>
2021-03-31 21:17:51 +0000

    WIP: Saving while things are kind of working, still need to add plot publisher code

--- src/ourbotmanager/ourbotmanager/base_motor_controller.py
+++ src/ourbotmanager/ourbotmanager/base_motor_controller.py
@@ -4,14 +4,27 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist,Pose,Point,Quaternion
 import serial
 import os
+import math
 
 class BaseMotorController(Node):
     SERIAL_PORT = '/dev/ttyUSB0'
+
     PWD_MIN = 10 # Minumum PWD supported by base
     LINEAR_TO_PWD = 100 # if linear.x = 1 m/s then set speed (pwd) to 100
     ANGULAR_TO_PWD = 90 # if angular.z = 1 rad/s then set speed (pwd) to 100
 
-    motionInfo = ""
+    # Keep a running plot of location and orentation of the base
+    # as feedback is returned from the base using meters and radians
+    plot_x = 0
+    plot_y = 0
+    plot_angle = 0
+
+    # Conversion factors for converting the base_feedback info that is in motor encoder ticks
+    # to meters and radians used by the plot message
+    TICKS_TO_METERS = 0.001
+    TICKS_TO_RADIANS = 0.01
+
+    base_feedback = ""
     
     def __init__(self):
         super().__init__("base_motor_controller") 
@@ -65,11 +78,40 @@ class BaseMotorController(Node):
         readChr = self.ser.read(1).decode('utf-8')
         while len(readChr) > 0 :
             if readChr == "\n":
-                self.get_logger().info(self.motionInfo)
-                self.motionInfo = ""
+                self.get_logger().info(self.base_feedback)
+                self.process_plot()
+                self.base_feedback = ""
             else:
-                self.motionInfo += readChr
+                self.base_feedback += readChr
             readChr = self.ser.read(1).decode('utf-8')
+    
+    def process_plot(self):
+        # Calculate location and angle of the base from the base feedback 
+        # and publish as a Plot message
+        if len(self.base_feedback) != 7:
+            self.get_logger().error("Misformed base_feedback message:" + self.base_feedback)
+        else:
+            baseMode = self.base_feedback[0:1]
+            if baseMode not in ["R","T"]:
+                self.get_logger().error("Misformed base_feedback baseMode :" + baseMode)
+            else:
+                ticks_string = self.base_feedback[1:7]
+                if not ticks_string.isnumeric:
+                    self.get_logger().error("Misformed base_feedback ticks value :" + ticks_string)
+                else:
+                    ticks = int(ticks_string)
+                    if baseMode == "T":
+                        self.plot_angle += (self.TICKS_TO_RADIANS * ticks)
+                        self.publish_Plot()
+                    else:
+                        # Calculate plot_x and plot_y co-ordinants based on current location, plot_angle and distance moved
+                        self.plot_x += (self.TICKS_TO_METERS * ticks) * math.cos(self.plot_angle)
+                        self.plot_y += (self.TICKS_TO_METERS * ticks) * math.sin(self.plot_angle)
+                        self.publish_Plot()
+                        
+    def publish_Plot(self):
+        self.get_logger().info("plot_x:" + str(self.plot_x) + " plot_y:" + str(self.plot_y) + " plot_angle:" + str(self.plot_angle))
+        
 
 def main(args=None):
     rclpy.init(args=args)
