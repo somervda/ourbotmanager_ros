4fd51a711c4e593db6ddf2c80204ee4542240ca7
somervda <davidsomerville@comcast.net>
2021-04-08 15:54:08 +0000

    ADC1115 library version of the joy_node

--- src/ourbotmanager/ourbotmanager/joy_node.py
+++ src/ourbotmanager/ourbotmanager/joy_node.py
@@ -4,44 +4,42 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from smbus2 import SMBus
 import time
+# Import the ADS1x15 module.
+import Adafruit_ADS1x15
 
 
 class JoyNode(Node): 
-    # RPi Channel 1
-    channel = 1
-    # ADS1115 address and registers
-    address = 0x48
-    reg_config = 0x01
-    reg_conversion = 0x00
+    
+    GAIN = 1
 
     def __init__(self):
         super().__init__("joy_node") 
+        try:
+            self.adc = Adafruit_ADS1x15.ADS1115( busnum=1)
+        except:
+            self.get_logger().error("Unexpected error opening adc:", sys.exc_info()[0])
+            rclpy.shutdown()
         self.pub = self.create_publisher(Twist,"base/twist",10)
         # Fire up an asyncronous timer to check for messages from the ft232 on SERIAL_PORT
-        self.joy_timer = self.create_timer(0.1 , self.process_joy) 
-        self.bus = SMBus(self.channel)
+        self.joy_timer = self.create_timer(1.0 , self.process_joy) 
         self.get_logger().info("joy_node has started")
 
     def process_joy(self):
-        # Check the joystick hardware status,send a twist message when needed
-        config = [0x83, 0xC3]
-        # Start conversion
-        self.bus.write_i2c_block_data(self.address, self.reg_config,config)
-        # Wait for conversion
-        time.sleep(0.01)
-        # Read 16-bit result
-        result = self.bus.read_i2c_block_data(self.address, self.reg_conversion, 2)
-        self.get_logger().info(f"AO value: {self.tc_to_int(result)}")
-
-
-    def tc_to_int(self,tcIn):
-        # Convert from 2-complement to an integer
-        value = ((tcIn[0] & 0xFF) << 8) | (tcIn[1] & 0xFF)
-        if value & 0x8000 != 0:
-            value -= 1 << 16
-        return value
-        
-
+        values = [0]*4
+        for i in range(4):
+            # Read the specified ADC channel using the previously set gain value.
+            try:
+                values[i] = self.adc.read_adc(i, gain=self.GAIN)
+            except:
+                self.get_logger().error("Error reading ADC")
+            # Note you can also pass in an optional data_rate parameter that controls
+            # the ADC conversion time (in samples/second). Each chip has a different
+            # set of allowed data rate values, see datasheet Table 9 config register
+            # DR bit values.
+            #values[i] = adc.read_adc(i, gain=GAIN, data_rate=128)
+            # Each value will be a 12 or 16 bit signed integer value depending on the
+            # ADC (ADS1015 = 12-bit, ADS1115 = 16-bit).
+        self.get_logger().info("Values:" + str(values[0]) + ", " + str(values[1]))
 
 def main(args=None):
     rclpy.init(args=args)
--- src/ourbotmanager/setup.py
+++ src/ourbotmanager/setup.py
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "base_motor_controller=ourbotmanager.base_motor_controller:main"
+            "base_motor_controller=ourbotmanager.base_motor_controller:main",
+            "joy_node=ourbotmanager.joy_node:main",
         ], 
     },
 )
